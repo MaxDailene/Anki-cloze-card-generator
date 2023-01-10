dbe90bb412aa76b52b6b90d261dbd2e841e6a39e
MaxDailene <57303798+MaxDailene@users.noreply.github.com>
2023-01-10 20:20:40 +0100

    Add files via upload

---
+++ AddText.py
@@ -0,0 +1,13 @@
+import csv
+
+
+input_file = 'input.csv'
+output_file = 'output.csv'
+add_text = 'Text to add: '
+
+with open(input_file, 'r') as csv_in:
+    reader = csv.reader(csv_in)
+    with open(output_file, 'w') as txt_out:
+        for row in reader:
+            sentence = add_text + row[0]
+            txt_out.write(sentence + '\n')
