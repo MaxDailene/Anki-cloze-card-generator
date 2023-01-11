c0559e46b159f6f135773915afcd84e238e004a3
MaxDailene <57303798+MaxDailene@users.noreply.github.com>
2023-01-11 11:40:34 +0100

    Update AddText.py

--- AddText.py
+++ AddText.py
@@ -5,7 +5,7 @@ input_file = 'input.csv'
 output_file = 'output.csv'
 add_text = 'Text to add: '
 
-with open(input_file, 'r') as csv_in:
+with open(input_file, 'r', encoding='utf-8') as csv_in:
     reader = csv.reader(csv_in)
     with open(output_file, 'w') as txt_out:
         for row in reader:
