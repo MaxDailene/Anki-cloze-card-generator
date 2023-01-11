6b9efca326131560d5fb5b6c43f29153ed469d6e
MaxDailene <57303798+MaxDailene@users.noreply.github.com>
2023-01-11 11:44:42 +0100

    Update AddText.py

--- AddText.py
+++ AddText.py
@@ -7,7 +7,7 @@ add_text = 'Text to add: '
 
 with open(input_file, 'r', encoding='utf-8') as csv_in:
     reader = csv.reader(csv_in)
-    with open(output_file, 'w') as txt_out:
+    with open(output_file, 'w', encoding='utf-8') as txt_out:
         for row in reader:
             sentence = add_text + row[0]
             txt_out.write(sentence + '\n')
