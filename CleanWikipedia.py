a68d60ef6097d48859783da6cf49ab9d0c3f3bc6
MaxDailene <57303798+MaxDailene@users.noreply.github.com>
2023-01-11 15:16:03 +0100

    Create CleanWikipedia.py

---
+++ CleanWikipedia.py
@@ -0,0 +1,13 @@
+import re
+
+def clean_text(file_path):
+    with open(file_path, "r") as file:
+        text = file.read()
+
+    cleaned_text = re.sub(r'\[.*?\]', '', text)
+
+    output_path = "cleaned_" + file_path
+    with open(output_path, "w") as output_file:
+        output_file.write(cleaned_text)
+
+clean_text("input.txt")
