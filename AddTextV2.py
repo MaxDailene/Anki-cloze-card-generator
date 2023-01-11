d4b1723ea026fb21c7a40b8a9d830eba3f013164
MaxDailene <57303798+MaxDailene@users.noreply.github.com>
2023-01-11 11:41:38 +0100

    Update and rename AddTextV2 to AddTextV2.py

--- AddTextV2.py
+++ AddTextV2.py
@@ -3,12 +3,12 @@ import csv
 input_file = 'input.csv'
 add_text = 'Text to add: '
 
-with open(input_file, 'r') as csv_in:
+with open(input_file, 'r', encoding='utf-8') as csv_in:
     reader = csv.reader(csv_in)
     # read all the rows from the csv
     rows = [row for row in reader]
 
-with open(input_file, 'w') as csv_out:
+with open(input_file, 'w', encoding='utf-8') as csv_out:
     writer = csv.writer(csv_out)
     for row in rows:
         sentence = add_text + row[0]
