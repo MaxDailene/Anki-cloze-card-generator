46e6c99de83377dd9d199f8d43646209da6afd5b
MaxDailene <57303798+MaxDailene@users.noreply.github.com>
2023-01-13 19:10:04 +0100

    Update CleanWikipedia.py

--- CleanWikipedia.py
+++ CleanWikipedia.py
@@ -1,4 +1,5 @@
 import re
+import os
 
 def clean_text(file_path):
     with open(file_path, "r") as file:
@@ -11,3 +12,5 @@ def clean_text(file_path):
         output_file.write(cleaned_text)
 
 clean_text("input.txt")
+os.remove("input.txt")
+os.rename("cleaned_input.txt", "input.txt")
