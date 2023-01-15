2eaeea6a8ed00750470162ce2304916f1db64835
MaxDailene <57303798+MaxDailene@users.noreply.github.com>
2023-01-15 11:13:43 +0100

    Update PrettifyV3.py

--- PrettifyV3.py
+++ PrettifyV3.py
@@ -1,9 +1,9 @@
 from bs4 import BeautifulSoup
 
-with open("file.txt") as f:
+with open("input.txt") as f:
     soup = BeautifulSoup(f, "lxml")
 
 text = soup.get_text()
 
-with open("file.txt", "w", encoding='utf-8') as out:
+with open("input.txt", "w", encoding='utf-8') as out:
     out.write(text)
