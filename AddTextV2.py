02956a1e09dc4ea82f238725bb6a4556d8b6ffc5
MaxDailene <57303798+MaxDailene@users.noreply.github.com>
2023-01-19 11:29:16 +0100

    Update AddTextV2.py

--- AddTextV2.py
+++ AddTextV2.py
@@ -2,7 +2,7 @@ import csv
 
 input_file = 'input.txt'
 output_file = 'output.txt'
-add_text = '<b>A pártellenzék erősödése<br>Hogyan képzelték el a rendszer megújítását?</b><br>'
+add_text = '<b>p<br>'
 
 with open(input_file, 'r', encoding='utf-8') as txt_in:
     with open(output_file, 'w', encoding='utf-8') as txt_out:
