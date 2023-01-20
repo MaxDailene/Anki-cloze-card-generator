b8fb141b7cda643a8f6e68582596afdc98ee2a14
MaxDailene <57303798+MaxDailene@users.noreply.github.com>
2023-01-20 14:33:30 +0100

    Update NewPath.py

--- NewPath.py
+++ NewPath.py
@@ -18,12 +18,14 @@ for sentence in sentences:
         sentence = sentence.split(start_char)
         before, after = sentence[0], sentence[1]
         after = re.sub(r"[\/\\\"]", "", after)
+        after = after.lstrip()
+        after = after.replace("  ", " ")
         words = after.split()
         for i, word in enumerate(words):
             if word.isnumeric():
                 counter += 1
                 words[i] = word[0] + "{{c" + str(counter) + "::" + word[1:] + "}}"
-            elif len(word) <= 3:
+            elif len(word) <= 5:
                 continue
             else:
                 counter += 1
@@ -33,7 +35,10 @@ for sentence in sentences:
         clozed_sentences.append(sentence)
     counter = 0
 
+
+...
 with open("output.csv", "w", newline='', encoding='utf-8') as file:
-    writer = csv.writer(file)
+    clozed_sentences = [re.sub(r"\s+", " ", sentence) for sentence in clozed_sentences]
     clozed_sentences = clozed_sentences[:-1]
+    writer = csv.writer(file)
     writer.writerows([[s] for s in clozed_sentences])
