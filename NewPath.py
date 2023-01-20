fb8f679680041a5beee947d3ba9520bcf27c96c6
MaxDailene <57303798+MaxDailene@users.noreply.github.com>
2023-01-20 14:08:01 +0100

    Create NewPath.py

---
+++ NewPath.py
@@ -0,0 +1,39 @@
+import nltk
+import re
+import csv
+
+start_char = "#"
+
+with open("input.txt", "a", encoding='utf-8') as file:
+    file.write('\n.')
+with open("input.txt", "r", encoding='utf-8') as file:
+    text = file.read()
+
+sentences = nltk.sent_tokenize(text)
+
+clozed_sentences = []
+counter = 0
+for sentence in sentences:
+    if start_char in sentence:
+        sentence = sentence.split(start_char)
+        before, after = sentence[0], sentence[1]
+        after = re.sub(r"[\/\\\"]", "", after)
+        words = after.split()
+        for i, word in enumerate(words):
+            if word.isnumeric():
+                counter += 1
+                words[i] = word[0] + "{{c" + str(counter) + "::" + word[1:] + "}}"
+            elif len(word) <= 3:
+                continue
+            else:
+                counter += 1
+                words[i] = word[0] + "{{c" + str(counter) + "::" + word[1:] + "}}"
+        clozed_sentences.append(before + " " + " ".join(words))
+    else:
+        clozed_sentences.append(sentence)
+    counter = 0
+
+with open("output.csv", "w", newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    clozed_sentences = clozed_sentences[:-1]
+    writer.writerows([[s] for s in clozed_sentences])
