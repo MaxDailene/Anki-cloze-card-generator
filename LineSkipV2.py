a05cc5c14f5fe4b4fbb0fd7d101b6cc56d54d0ec
MaxDailene <57303798+MaxDailene@users.noreply.github.com>
2023-02-10 13:34:06 +0100

    Update LineSkipV2.py

--- LineSkipV2.py
+++ LineSkipV2.py
@@ -1,8 +1,8 @@
 def LineSkip(file_path, n):
-    with open(file_path, "r") as file:
+    with open(file_path, "r", encoding='utf-8') as file:
         lines = file.readlines()
         
-    with open(file_path, "w") as file:
+    with open(file_path, "w", encoding='utf-8') as file:
         for i, line in enumerate(lines):
             if (i + 1) % n != 0:
                 file.write(line)
