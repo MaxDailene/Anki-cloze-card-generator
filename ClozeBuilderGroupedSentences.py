be90267c112fc0f887d5dbf31b160f078cd1345d
MaxDailene <57303798+MaxDailene@users.noreply.github.com>
2023-02-19 16:25:47 +0100

    Update ClozeBuilderGroupedSentences.py

--- ClozeBuilderGroupedSentences.py
+++ ClozeBuilderGroupedSentences.py
@@ -12,30 +12,25 @@ with open("input.txt", "r", encoding='utf-8') as file:
 sentences = nltk.sent_tokenize(text)
 
 clozed_sentences = []
-counter = 0
-current_sentence_index = 0
-while current_sentence_index < len(sentences):
-    words_list = []
-    for j in range(n):
-        if current_sentence_index >= len(sentences):
-            break
-        sentence = sentences[current_sentence_index]
+for i in range(0, len(sentences), n):
+    group = sentences[i:i+n]
+    group_clozes = []
+    counter = 0
+    for sentence in group:
         sentence = re.sub(r"[\/\\\";]", "", sentence)
         words = sentence.split()
         for i, word in enumerate(words):
             if word.isnumeric():
                 counter += 1
                 words[i] = "{{c" + str(counter) + "::" + word + "}}"
-            elif len(word) <= 7:
+            elif len(word) <= 3:
                 continue
             else:
                 counter += 1
                 words[i] = "{{c" + str(counter) + "::" + word + "}}"
-        words_list.append(" ".join(words))
-        current_sentence_index += 1
-        counter = 0
-    clozed_sentences.append(" ".join(words_list))
-
+        group_clozes.append(" ".join(words))
+    clozed_sentences.append(" ".join(group_clozes))
+        
 with open("output.csv", "w", newline='', encoding='utf-8') as file:
     writer = csv.writer(file)
     writer.writerows([[s] for s in clozed_sentences])
