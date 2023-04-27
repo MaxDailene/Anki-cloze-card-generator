9ed952d846f2ee3a47a678011c9fdcb66d32cce8
MaxDailene <57303798+MaxDailene@users.noreply.github.com>
2023-04-27 23:07:50 +0200

    Update VocabScribe.py

--- VocabScribe.py
+++ VocabScribe.py
@@ -2,28 +2,22 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-# Enter the Vocabulary.com link
 url = input("Enter Vocabulary.com link: ")
 
-# Get the HTML content of the webpage
 response = requests.get(url)
 html_content = response.text
 
-# Parse the HTML content using BeautifulSoup
 soup = BeautifulSoup(html_content, 'html.parser')
 
-# Find all words and their respective definitions
 words = soup.find_all('a', class_='word')
 definitions = soup.find_all('div', class_='definition')
 
-# Create a list to store the words and their definitions
 word_list = []
 for i in range(len(words)):
     word = re.sub(r'\s+', ' ', words[i].text.strip())
     definition = re.sub(r'\s+', ' ', definitions[i].text.strip())
     word_list.append(word + '\t' + definition)
 
-# Write the words and their definitions to a text file
 filename = re.sub('[^\w\s-]', '', soup.title.text.strip())
 filename = filename.replace(' ', '_') + '.txt'
 with open(filename, 'w', encoding='utf-8') as f:
