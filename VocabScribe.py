641974b4da5b80c96267a2d33e753b139ed6e231
MaxDailene <57303798+MaxDailene@users.noreply.github.com>
2023-08-25 16:06:24 +0200

    Update VocabScribe.py

--- VocabScribe.py
+++ VocabScribe.py
@@ -6,96 +6,84 @@ import time
 import os
 import random
 
-webdriver_path = 'Chrome'
-
-driver = webdriver.Chrome(service=Service(webdriver_path))
-
-url = input("Enter Vocabulary.com link: ")
-
-driver.get(url)
-
-html_content = driver.page_source
-
-soup = BeautifulSoup(html_content, 'html.parser')
-
-wordlist = soup.find_all('li', class_='wordlist')
-
-if wordlist:
+def scrape_vocabulary_links(url):
+    driver = webdriver.Chrome()
+    driver.get(url)
+    
+    html_content = driver.page_source
+    soup = BeautifulSoup(html_content, 'html.parser')
+    
     links = []
-    for li in wordlist:
-        header = li.find('div', class_='header')
-        if header:
-            h2 = header.find('h2')
-            if h2 and h2.find('a'):
-                link = "https://www.vocabulary.com/" + h2.find('a')['href']
-                links.append(link)
 
-    with open('Links.txt', 'w', encoding='utf-8') as f:
-        f.write('\n'.join(links))
+    wordlist = soup.find_all('li', class_='wordlist')
+    if wordlist:
+        for li in wordlist:
+            header = li.find('div', class_='header')
+            if header:
+                h2 = header.find('h2')
+                if h2 and h2.find('a'):
+                    link = "https://www.vocabulary.com/" + h2.find('a')['href']
+                    links.append(link)
 
     driver.quit()
+    return links
 
-    with open('Links.txt', 'r') as f:
-        links = f.read().splitlines()
-
-    for link in links:
-        sleep_duration = random.randint(6, 15)
-        time.sleep(sleep_duration)
-        driver = webdriver.Chrome(service=Service(webdriver_path))
-        driver.get(link)
-        html_content = driver.page_source
-        soup = BeautifulSoup(html_content, 'html.parser')
-        words = soup.find_all('a', class_='word')
-        definitions = soup.find_all('div', class_='definition')
-        if soup.find('div', class_='description'):
-           examples = soup.find_all('div', class_='description')
-        else:
-           examples = soup.find_all('div', class_='example')
-        word_list = []
-        for i in range(len(words)):
-            word = re.sub(r'\s+', ' ', words[i].text.strip())
-            definition = re.sub(r'\s+', ' ', definitions[i].text.strip())
-            example_div = examples[i] if i < len(examples) else None
-            example_a = example_div.find('a', class_='source') if example_div else None
-            if example_a:
-                example_a.extract()
-            example = re.sub(r'\s+', ' ', example_div.text.strip()) if example_div else ''
-            word_list.append(word + '\t' + definition + '\t' + example)
-        filename = re.sub('[^\w\s-]', '', soup.title.text.strip())
-        filename = filename.replace(' ', '_') + '.txt'
-        with open(filename, 'w', encoding='utf-8') as f:
-            f.write('\n'.join(word_list))
-        print(f"Words, definitions, and examples have been written to {filename}.")
-        driver.quit()
-else:
-    words = soup.find_all('a', class_='word')
-    definitions = soup.find_all('div', class_='definition')
-    if soup.find('div', class_='description'):
-      examples = soup.find_all('div', class_='description')
-    else:
-      examples = soup.find_all('div', class_='example')
+def scrape_word_data(link):
+    driver = webdriver.Chrome()
+    driver.get(link)
+    
+    html_content = driver.page_source
+    soup = BeautifulSoup(html_content, 'html.parser')
+    
     word_list = []
-    for i in range(len(words)):
-        word = re.sub(r'\s+', ' ', words[i].text.strip())
-        definition = re.sub(r'\s+', ' ', definitions[i].text.strip())
-        example_div = examples[i] if i < len(examples) else None
+
+    word_elements = soup.find_all('a', class_='word')
+    definition_elements = soup.find_all('div', class_='definition')
+
+    if soup.find('div', class_='description'):
+        example_elements = soup.find_all('div', class_='description')
+    else:
+        example_elements = soup.find_all('div', class_='example')
+
+    for i in range(len(word_elements)):
+        word = re.sub(r'\s+', ' ', word_elements[i].text.strip())
+        definition = re.sub(r'\s+', ' ', definition_elements[i].text.strip())
+        example_div = example_elements[i] if i < len(example_elements) else None
         example_a = example_div.find('a', class_='source') if example_div else None
         if example_a:
             example_a.extract()
         example = re.sub(r'\s+', ' ', example_div.text.strip()) if example_div else ''
         word_list.append(word + '\t' + definition + '\t' + example)
-    filename = re.sub('[^\w\s-]', '', soup.title.text.strip())
-    filename = filename.replace(' ', '_') + '.txt'
+
+    filename = re.sub('[^\w\s-]', '', soup.title.text.strip()).replace(' ', '_') + '.txt'
     with open(filename, 'w', encoding='utf-8') as f:
         f.write('\n'.join(word_list))
-    print(f"Words, definitions, and examples have been written to {filename}.")
-driver.quit()
-batch_filename = 'OneFile.txt'
-with open(batch_filename, 'w', encoding='utf-8') as batch_file:
-    for filename in os.listdir():
-        if filename.endswith('.txt') and 'Vocabulary_List' in filename:
-            with open(filename, 'r', encoding='utf-8') as f:
-                batch_file.write(f.read().strip() + '\n')
 
-if os.path.exists('Links.txt'):
-    os.remove('Links.txt')
+    driver.quit()
+    return filename
+
+def main():
+    url = input("Enter Vocabulary.com link: ")
+
+    links = scrape_vocabulary_links(url)
+    
+    if links:
+        for link in links:
+            filename = scrape_word_data(link)
+            print(f"Words, definitions, and examples have been written to {filename}.")
+    else:
+        filename = scrape_word_data(url)
+        print(f"Words, definitions, and examples have been written to {filename}.")
+    
+    batch_filename = 'OneFile.txt'
+    with open(batch_filename, 'w', encoding='utf-8') as batch_file:
+        for filename in os.listdir():
+            if filename.endswith('.txt') and 'Vocabulary_List' in filename:
+                with open(filename, 'r', encoding='utf-8') as f:
+                    batch_file.write(f.read().strip() + '\n')
+
+    if os.path.exists('Links.txt'):
+        os.remove('Links.txt')
+
+if __name__ == "__main__":
+    main()
